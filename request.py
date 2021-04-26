5a159e5c7b35f4ebbafc480f7999995b1422885a
Luis625 <luisfelipec625@gmail.com>
2021-04-26 16:22:59 -0300

    Add files via upload

--- request.py
+++ request.py
@@ -1,6 +1,21 @@
 import json
 import requests
 import pandas as pd
+import gspread
+
+from container import Container
+from oauth2client.service_account import ServiceAccountCredentials
+
+#Auth Google Sheets       
+scope = ['https://spreadsheets.google.com/feeds']
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name('mimetic-parity-311801-01a924f481ab.json', scope)
+
+gc = gspread.authorize(credentials)
+
+wks = gc.open_by_key('1XL8hE1ve3aFKS_Gu0fIwQhNN2BXPnBr0Xv7x9rkrDqY')
+
+worksheet = wks.get_worksheet(0)
 
 
 url = "http://uiot-dims.herokuapp.com/list/data"
@@ -9,62 +24,37 @@ r = requests.get(url)
 
 
 content = r.json()
-#CREATE DF
+# CREATE DF
 df = pd.DataFrame.from_dict(content)
-
-# some df observations
-#print(df.head)
-#df.info()
-#print(df.describe())
-
 
 # TODO add more devices according to chipset
 # TODO create link between chipset and device (chipset x = id y)
 
-# CREATE dataframe matheus - CURRENT DEVICE 
-dfmath = df[df["chipset"]== "AE:08:62:24:F9:71"]
-#print(dfmath.head())
 
-# select last 2 rows (before that the sendings wasn't aggregated)
-dfmath = dfmath.iloc[0:2,:]
-print(dfmath)
-#print(type(dfmath))
+# Another way is to call function "dfmath = df.where(df["chipset"] == "AE:08:62:24:F9:71")"
+# CREATE dataframe matheus - CURRENT DEVICE
+dfmath = df[df["chipset"] == "AE:08:62:24:F9:71"]
+dffake = df[df["chipset"] == "GREEN DASHBOARD TEST"]
 
-
-#TODO split values into columns
-
-"""
-#OPTION 1
-
-dfmath = dfmath.str.split(expand=True,)
-print(dfmath)
-# AttributeError: 'DataFrame' object has no attribute 'str'
-"""
-
-"""
-#OPTION 2
-
-#convert to series
-dfmath = pd.Series(dfmath.value.values.flatten())
-print(dfmath)
-print(type(dfmath))
-
-dfmath = dfmath.str.split("/", expand=True)
-print(dfmath)
-
-#nan value
-"""
-
-#EXPORTING TO CSV 
-# no unziping
-dfmath.to_csv('out.csv',index=False) 
-
-# if you want to zip it
-"""
-compression_opts = dict(method='zip',archive_name='out.csv')
-dfmath.to_csv('out.zip', index=False,compression=compression_opts)  
-"""
+# Acquisitions with date and time format different than "21/04/20" and "0:11:20" will not work
+dfmath = dfmath.iloc[10:13]
 
 
-#TODO export to google sheets
+
+# 'Container.to_container(df)' method gets the array and returns a container object
+instancy_math = Container.to_container(dfmath)
+instancy_fake = Container.to_container(dffake)
+
+# Since both instancies are arrays, you can either give them separated or concatenated
+# Pandas.DataFrame has a lot of useful methods, like "to_csv" and "to_excel".
+# This function gets the 'container' array and returns a DataFrame object
+full_dataFrame = Container.array_to_dataFrame(instancy_math + instancy_fake)
+print(full_dataFrame)
+
+worksheet.update([full_dataFrame.columns.values.tolist()] + full_dataFrame.values.tolist())
+
+# Exporting to .csv and .xlsx files without zipping
+full_dataFrame.to_csv("output.csv", index = False)
+full_dataFrame.to_excel("output.xlsx")
+
 
